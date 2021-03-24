6cb676ce4976d2aee2597660c49d88e4fba71578
Crystal Atkinson <crystaltayloratkinson@gmail.com>
2021-03-24 09:48:27 -0400

    Fixed sql error

--- models/usersModel.js
+++ models/usersModel.js
@@ -13,7 +13,7 @@ class User {
   static async checkUserNames(username) {
     try {
       const response = await db.any(
-        `SELECT username FROM users WHERE username=${username};`
+        `SELECT username FROM users WHERE username='${username}';`
       );
       return true;
     } catch (err) {
